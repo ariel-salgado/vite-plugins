0523a6e843c82d40aedc8e20524ac27ed103518a
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-02-27 17:29:20 -0300

    chore: ignore fixtures

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from '@ariel-salgado/eslint-config';
 const config = defineConfig({
 	type: 'lib',
 	typescript: true,
+	ignores: [
+		'**/tests/**/fixtures/**',
+	],
 });
 
 export default config;
