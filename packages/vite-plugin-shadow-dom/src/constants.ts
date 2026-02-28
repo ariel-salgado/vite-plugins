067a6b546f1421545911bc33f53fd65dbcb49012
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-02-27 21:44:42 -0300

    feat: formatOutput enabled by default

--- packages/vite-plugin-shadow-dom/src/constants.ts
+++ packages/vite-plugin-shadow-dom/src/constants.ts
@@ -28,6 +28,7 @@ export const DEFAULT_PLUGIN_OPTIONS: Partial<ResolvedOptions> = {
 	appId: 'app',
 	shadowRootGlobal: '__shadowRoot',
 	patchDocument: true,
+	formatOutput: true,
 };
 
 export const DOCUMENT_METHODS_TO_PATCH = [
