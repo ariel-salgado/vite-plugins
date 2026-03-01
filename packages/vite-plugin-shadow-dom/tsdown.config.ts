d43ac8a5da94e82b14932f88c4a8c66a492014fb
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-03-01 00:41:40 -0300

    fix: generate different builds for environments

--- packages/vite-plugin-shadow-dom/tsdown.config.ts
+++ packages/vite-plugin-shadow-dom/tsdown.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-	entry: {
-		index: 'src/index.ts',
-		dom: 'src/dom/index.ts',
+export default defineConfig([
+	{
+		entry: {
+			index: 'src/index.ts',
+		},
+		format: ['esm'],
+		shims: true,
+		dts: true,
+		clean: true,
+		exports: true,
+		platform: 'node',
 	},
-	format: ['esm'],
-	dts: true,
-	clean: true,
-	platform: 'node',
-	treeshake: true,
-});
+	{
+		entry: {
+			client: 'src/client/index.ts',
+		},
+		format: ['esm'],
+		shims: true,
+		dts: true,
+		clean: true,
+		exports: true,
+		platform: 'browser',
+	},
+]);
