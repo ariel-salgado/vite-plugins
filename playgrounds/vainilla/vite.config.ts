a1d6cc5e3a8899b8a229dcda61ebc2b064db0bdd
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2026-03-05 10:12:07 -0300

    chore: default vanilla vite template

--- playgrounds/vainilla/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { shadowDOM } from '@ariel-salgado/vite-plugin-shadow-dom';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	appType: 'mpa',
-	plugins: [
-		shadowDOM(),
-	],
-});
